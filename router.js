8fdbb82ecca0a3433470f16b9b2f4ff3cc515aef
Paul Ogawa <paul.ogawa7@gmail.com>
2015-06-12 14:39:51 -0700

    Add cleanup

--- router.js
+++ router.js
@@ -1,5 +1,4 @@
 QandA.Router.map(function() {
-  this.resource('answers');
   this.resource('answer', {path: 'answer/:answer_id'})
   this.resource('questions', {path: '/'});
   this.resource('question', {path: 'question/:question_id'}, function(){
